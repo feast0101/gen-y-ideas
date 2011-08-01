63e65bbdfa5d51781f600b751b84b460e4c1f576
kallolds <kallolds@0641896a-cf80-20a5-cf12-2247dfea9a1a>
2011-08-01 17:20:30 +0000

    Committing Picasa demo files

---
+++ trunk/src/com/kd/picasa/TestPicasa.java
@@ -0,0 +1,26 @@
+package com.kd.picasa;
+import java.io.File;
+import java.net.URL;
+
+import com.google.gdata.client.*;
+import com.google.gdata.client.photos.*;
+import com.google.gdata.data.*;
+import com.google.gdata.data.media.*;
+import com.google.gdata.data.photos.*;
+public class TestPicasa {
+	
+	PicasawebService myService = new PicasawebService("exampleCo-exampleApp-1");
+	//myService.setUserCredentials("kallolds@gmail.com", "pass007");
+/*	URL feedUrl = new URL("https://picasaweb.google.com/data/feed/api/user/kallolds?kind=album");
+
+	UserFeed myUserFeed = myService.getFeed(feedUrl, UserFeed.class);
+
+	for (AlbumEntry myAlbum : myUserFeed.getAlbumEntries()) {
+	    System.out.println(myAlbum.getTitle().getPlainText());
+	}*/
+	
+	//Add an album
+	
+
+
+}
